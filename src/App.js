f389d0763e107e3c1aa1a41d07ccb87c1b1ca93f
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-01-01 09:45:18 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nataliia-boiko-ab48a9242/"
+          target="_blank">
+          Nataliia Boiko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NataliiaBoiko22/react-weather-app"
+          target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
